c24fadd9bdf9fc180e68783cd73adec7a1443168
nee-tin <nkrkey@gmail.com>
2026-05-11 21:32:07 +0545

    Delete html-version directory.yes

--- html-version/script.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.getElementById("menu-toggle");
-const nav = document.getElementById("nav-links");
-
-toggle.addEventListener("click", () => {
-  nav.classList.toggle("active");
-});
